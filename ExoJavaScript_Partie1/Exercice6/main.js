0f3edce1bc0db443480f2c410a3eaa4d6935188f
Emilie-Rose <78940316+Emilie-Rose@users.noreply.github.com>
2021-04-06 15:57:09 +0200

    créa partie 3 et 4

---
+++ ExoJavaScript_Partie1/Exercice6/main.js
@@ -0,0 +1,10 @@
+function age() {
+  var age = prompt("veuillez entrer votre âge");
+  if (age >= 18){
+      alert ("vous êtes majeur");
+  } else{
+      alert ("vous êtes mineur");
+    }
+//    var number = document.getElementById ('mult1').value;
+}
+age();
---
+++ ExoJavaScript_Partie2/Exercice1/main.js
@@ -0,0 +1,12 @@
+//image1.onmouseover = function() {
+//image1.src="img/image5.jpg"
+//};
+
+// image1.onmouseout = function() {
+//     image1.src="img/image4_2.jpg"
+// };
+
+// JavaScript Document
+imageOne.onmouseover = function() {
+  imageOne.src="imageB.jfif";
+}
