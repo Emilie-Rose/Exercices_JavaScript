5ec16ce64683c9caf224c8fccc74321e8346d6fc
Emilie-Rose <78940316+Emilie-Rose@users.noreply.github.com>
2021-04-07 22:39:18 +0200

    maj partie1

---
+++ ExoJavaScript_Partie1/Exercice_6/main.js
@@ -0,0 +1,10 @@
+function age() {
+  var age = prompt("Veuillez entrer votre âge");
+  if (age >= 18){
+      alert ("Vous êtes majeur");
+  } else{
+      alert ("Vous êtes mineur");
+    }
+//    var number = document.getElementById ('mult1').value;
+}
+age();
